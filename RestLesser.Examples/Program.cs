c1688d71d14c1ecae0a39c6e4b2c588eb85a58a0
Tim Horemans <tim.horemans@tenforce.com>
2025-11-22 15:08:57 +0100

    Made examples a console app

---
+++ RestLesser.Examples/Program.cs
@@ -0,0 +1,14 @@
+﻿using RestLesser.Examples.TvMaze;
+
+namespace RestLesser.Examples
+{
+    static class Program
+    {
+        public static void Main(params string[] args)
+        {
+            var tvMaze = new TvMazeClient();
+            var show = tvMaze.GetShow(1);
+            Console.WriteLine($"Name: {show.Name}");
+        }
+    }
+}
