c37464f6a240b42c1a100f2382e9377415c5b956
worstenbrood <worstenbrood@gmail.com>
2025-12-09 00:19:23 +0100

    Added XmlAdapter

---
+++ RestLesser/DataAdapters/XmlAdapter.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Net.Http.Headers;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RestLesser.DataAdapters
+{
+    /// <summary>
+    /// XML (de)serializer
+    /// </summary>
+    public class XmlAdapter : IDataAdapter
+    {
+        /// <inheritdoc/>
+        public MediaTypeWithQualityHeaderValue MediaTypeHeader { get; } =
+            new MediaTypeWithQualityHeaderValue("application/xml");
+
+        /// <summary>
+        /// Namespaces used for writing
+        /// </summary>
+        public readonly XmlSerializerNamespaces Namespaces =
+            new ();
+                
+        /// <inheritdoc/>
+        public T Deserialize<T>(string data)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var reader = new StringReader(data))
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+
+        /// <summary>
+        /// XML writer settings
+        /// </summary>
+        protected static readonly XmlWriterSettings XmlWriterSettings =
+            new XmlWriterSettings
+            {
+                OmitXmlDeclaration = true,
+                Indent = true,
+                IndentChars = new string(' ', 2)
+            };
+
+
+        /// <inheritdoc/>
+        public string Serialize<T>(T data)
+        {
+            using (var stream = new StringWriter())
+            {
+                using (var writer = XmlWriter.Create(stream, XmlWriterSettings))
+                {
+                    var serializer = new XmlSerializer(typeof(T));
+                    if (Namespaces.Count == 0)
+                    {
+                       Namespaces.Add(string.Empty, string.Empty);
+                    }
+                    serializer.Serialize(writer, data, Namespaces);
+                    return stream.ToString();
+                }
+            }
+        }
+    }
+}
